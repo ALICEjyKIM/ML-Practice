32069f14ffa757e0c3de0a7110195e514ebed6d3
jiyoon <nrkjy528@gmail.com>
2025-05-06 17:11:19 +0900

    completed class BasicBlock(nn.Module):

--- CIFAR10ResNet.py
+++ CIFAR10ResNet.py
@@ -55,15 +55,26 @@ for i in range(10):
 class BasicBlock(nn.Module):
     def __init__(self, in_planes, planes, stride = 1):
         super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size = 3, stride = stride, padding = 1, bias = False)
+        self.conv1 = nn.Conv2d(in_planes, planes, 
+                               kernel_size = 3, 
+                               stride = stride, 
+                               padding = 1, 
+                               bias = False)
         self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size = 3, stride = 1, padding = 1, bias = False)
+        self.conv2 = nn.Conv2d(planes, planes, 
+                               kernel_size = 3, 
+                               stride = 1, 
+                               padding = 1, 
+                               bias = False)
         self.bn2 = nn.BatchNorm2d(planes)
         
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != planes:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, planes, kernel_size = 1, stride = stride, bias = False),
+                nn.Conv2d(in_planes, planes, 
+                          kernel_size = 1, 
+                          stride = stride, 
+                          bias = False),
                 nn.BatchNorm2d(planes))
     
     def forward(self, x):
@@ -78,7 +89,11 @@ class ResNet(nn.Module):
         super(ResNet, self).__init__()
         self.in_planes = 16
         
-        self.conv1 = nn.Conv2d(3, 16, kernel_size = 3, stride = 1, padding = 1, bias = False)
+        self.conv1 = nn.Conv2d(3, 16,
+                               kernel_size = 3, 
+                               stride = 1, 
+                               padding = 1, 
+                               bias = False)
         self.bn1 = nn.BatchNorm2d(16)
         self.layer1 = self._make_layer(16, 2, stride = 1)
         self.layer2 = self._make_layer(32, 2, stride = 2)
