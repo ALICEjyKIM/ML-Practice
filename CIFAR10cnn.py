2ef8973c34da7b44a946aa2e63e32405af46efe1
jiyoon <nrkjy528@gmail.com>
2025-05-06 13:29:41 +0900

    modified model construction

--- CIFAR10cnn.py
+++ CIFAR10cnn.py
@@ -51,26 +51,37 @@ for i in range(10):
     plt.imshow(np.transpose(X_train[i], (1, 2, 0)))
     plt.title('Class: ' + str(y_train[i].item()))
 
-''' 6. Multi Layer Perceptron (MLP) 모델 설계하기 '''
-class Net(nn.Module):
+''' 6. Convolutional Neural Network (CNN) 모델 설계하기 '''
+class CNN(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
-        self.fc1 = nn.Linear(32 * 32 * 3, 512)
-        self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, 10)
-
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels = 3, out_channels = 8, kernel_size = 3, padding = 1)
+        self.conv2 = nn.Conv2d(in_channels = 8, out_channels = 16, kernel_size = 3, padding = 1)
+        self.pool = nn.MaxPool2d(kernel_size = 2, stride = 2)
+        self.fc1 = nn.Linear(8 * 8 * 16, 64)
+        self.fc2 = nn.Linear(64, 32)
+        self.fc3 = nn.Linear(32, 10)
+        
     def forward(self, x):
-        x = x.view(-1, 32 * 32 * 3)
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.pool(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.pool(x)
+        
+        x = x.view(-1, 8 * 8 * 16)
         x = self.fc1(x)
         x = F.relu(x)
         x = self.fc2(x)
         x = F.relu(x)
         x = self.fc3(x)
-        x = F.log_softmax(x, dim = 1)
-        return x 
+        x = F.log_softmax(x)
+        return x
 
-''' 7. Optimizer, Objective Function 설정하기 '''
-model = Net().to(DEVICE)
+
+
+model = CNN().to(DEVICE)
 optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
 criterion = nn.CrossEntropyLoss()
 
